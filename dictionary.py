8dd2f1bac0804658e49fdaf83f338c463cad9add
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-22 09:56:43 -0500

    add a dictionary of profanity

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+naughty_words = ['shit',
+                 'damn',
+                 'fuck',
+                 'ass',
+                 'dick',
+                 'stupid',
+                 'dumb',
+                 'pissed',
+                 'livid',
+                 'mad',
+                 'angry']
+
+naughty_word_pattern = '|'.join(naughty_words)
