6cf411743bd6bccf7a26f2d1b737d1ec5938324d
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-21 22:21:35 -0500

    add ability to get twitter access token from twitter

--- twitter.py
+++ twitter.py
@@ -1,13 +1,9 @@
 # twitter.py
 from os import getenv, path
 from base64 import b64encode
+import requests
 
 # https://dev.twitter.com/docs/auth/application-only-auth
-def twitter_credentials():
-    key = twitter_key()
-    secret = twitter_secret()
-    return b64encode(':'.join([key, secret]))
-
 def twitter_key():
     if path.isfile('secrets.py'):
         import secrets
@@ -21,3 +17,17 @@ def twitter_secret():
         return secrets.TWITTER_CONSUMER_SECRET
     else:
         return getenv('TWITTER_CONSUMER_SECRET')
+
+def twitter_credentials():
+    key = twitter_key()
+    secret = twitter_secret()
+    return b64encode(':'.join([key, secret]))
+
+def obtain_twitter_bearer_token():
+    url = 'https://api.twitter.com/oauth2/token'
+    headers = {'Authorization': ' '.join(['Basic', twitter_credentials()]),
+               'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+    body = 'grant_type=client_credentials'
+
+    response = requests.post(url, data=body, headers=headers)
+    return str(response.json()[u'access_token'])
